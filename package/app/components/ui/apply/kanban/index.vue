73997d9d918e709f62bc1918eade5ee92e7de1ef
Mamadou Talla DIA <ibntalla@gmail.com>
2026-03-16 05:45:07 +0000

    replace new Sortable by useSortable

--- package/app/components/ui/apply/kanban/index.vue
+++ package/app/components/ui/apply/kanban/index.vue
@@ -1,57 +1,48 @@
 <script lang="ts" setup>
-import { type Apply } from "~~/server/database/collections";
-import Sortable from "sortablejs";
-import { updateApplyStatus } from "~/tools/apply";
+import type { Apply, Job } from "~~/server/database/collections";
+import { useSortable } from "@vueuse/integrations/useSortable";
 
 const { jobId: jobID } = defineProps<{ jobId: string }>();
 const { job } = useJob(jobID);
+const applyStatus = ref(job.value.applyStatus);
 
 const emit = defineEmits<(e: "update", apply: Apply) => void>();
 const container = useTemplateRef("container");
 
-onMounted(setSortable);
-function setSortable() {
-  if (!container.value) return;
+const {} = useSortable(container, applyStatus, {
+  handle: ".grid-handler",
+  disabled: Store.session.user?.role !== "admin",
+  group: { name: "grid", pull: false, put: false },
+  scroll: true,
+  dragClass: "dragging",
+  chosenClass: "choose",
 
-  new Sortable(container.value, {
-    scroll: true,
-    handle: ".grid-handler",
-    group: {
-      name: "grid",
-      pull: false,
-      put: false,
-    },
-    dragClass: "dragging",
-    chosenClass: "choose",
+  onChoose(event) {
+    event.item.setAttribute("data-grid-choosed", "");
+  },
+  onUnchoose(event) {
+    event.item.removeAttribute("data-grid-choosed");
+  },
+  onMove(evt) {
+    evt.dragged.setAttribute("data-grid-dragging", "");
+  },
 
-    onChoose(event) {
-      event.item.setAttribute("data-grid-choosed", "");
-    },
-    onUnchoose(event) {
-      event.item.removeAttribute("data-grid-choosed");
-    },
-    onMove(evt) {
-      evt.dragged.setAttribute("data-grid-dragging", "");
-    },
-    onEnd(evt) {
-      evt.item.removeAttribute("data-grid-dragging");
-
-      if (evt.oldIndex !== evt.newIndex) {
-        const newApplyStatus: any[] = [];
-
-        evt.target.querySelectorAll("[data-grid]").forEach((item) => {
-          const status = job.value.applyStatus.find(
-            (st) => st.key === item.getAttribute("data-grid"),
-          );
-
-          if (status) newApplyStatus.push(status);
+  async onEnd(evt) {
+    evt.item.removeAttribute("data-grid-dragging");
+    if (evt.oldIndex !== evt.newIndex) {
+      try {
+        const result = await Api.$fetch<Job>("/api/admin/job", {
+          method: "post",
+          body: { ...job.value, applyStatus: applyStatus.value },
         });
 
-        updateApplyStatus(job.value, newApplyStatus);
+        job.value = result;
+      } catch (error) {
+        console.error(error);
       }
-    },
-  });
-}
+    }
+  },
+});
 </script>
 
 <template>
@@ -62,7 +53,7 @@ function setSortable() {
     <ui-apply-kanban-grid :status="null" :job-id="jobID" />
 
     <div ref="container" class="flex gap-2">
-      <template v-for="status in job.applyStatus" :key="status.key">
+      <template v-for="status in applyStatus" :key="status.key">
         <ui-apply-kanban-grid v-model:status="status.key" :job-id="jobID" />
       </template>
 
--- package/grid.vue
+++ package/grid.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 import { type Apply } from "~~/server/database/collections";
 import type { IDataResult } from "~~/server/interfaces";
-import { useSortable } from "@vueuse/integrations/useSortable";
+import Sortable from "sortablejs";
+import _ from "lodash";
+import onFetchError from "~/tools/onFetchError";
 
 const { jobId: jobID } = defineProps<{ jobId: string }>();
 const status = defineModel<string | null>("status", { required: true });
@@ -20,8 +22,9 @@ const isFlip = useCookie<boolean>(`${job.value.id}_${status.value}`, {
 });
 
 const dayjs = useDayjs();
+const statusChanging = ref<Record<string, string>>({});
 const fetching = ref(false);
-const sContainer = useTemplateRef("sortableContainer");
+const container = useTemplateRef("container");
 
 const initFetching = ref(false);
 const openSearch = ref(false);
@@ -33,43 +36,6 @@ const result = ref<IDataResult<Apply>>();
 const items = ref<Apply[]>([]);
 const applys = useApplys();
 
-const {} = useSortable(sContainer, items, {
-  handle: ".handler",
-  group: "shared",
-  sort: false,
-  disabled: Store.session.user?.role === "guest",
-
-  chosenClass: "chosen",
-  dragClass: "drag",
-  ghostClass: "ghost",
-  swapClass: "swap",
-  selectedClass: "selected",
-  fallbackClass: "fallback",
-
-  async onAdd(event) {
-    const id = event.item.getAttribute("data-id");
-    if (!id) return;
-    if (typeof event.newIndex !== "number") return;
-
-    try {
-      const a0 = applys.get(id);
-      if (!a0) return;
-
-      items.value.splice(event.newIndex, 0, a0.apply.value);
-      await a0.status.value.submit(status.value ?? null);
-    } catch (error) {
-      console.error(error);
-    }
-  },
-
-  onRemove(event) {
-    const id = event.item.getAttribute("data-id");
-    if (!id) return;
-
-    items.value = items.value.filter((a) => a.id !== id);
-  },
-});
-
 onMounted(async () => {
   window.addEventListener(`${status.value}:actions`, (e: any) =>
     onEventActions(e),
@@ -78,6 +44,7 @@ onMounted(async () => {
 
   try {
     await getApplys();
+    setSortable();
   } catch (error) {
     console.log(error);
   } finally {
@@ -85,11 +52,35 @@ onMounted(async () => {
   }
 });
 
-function getIndex(id: string) {
-  const i = items.value.findIndex((a) => a.id === id);
-  if (i === -1) return null;
-  return i;
-}
+onBeforeUnmount(() => {
+  window.removeEventListener(`${status}:actions`, (e: any) =>
+    onEventActions(e),
+  );
+});
+
+watch(
+  () => items.value,
+  () => {
+    const _applys = items.value.filter(
+      (apply) => apply.status !== status.value,
+    );
+
+    for (const apply of _applys) {
+      dispatchEvent(
+        new CustomEvent(`${apply.status}:actions`, {
+          detail: { action: "apply:add", apply },
+        }),
+      );
+
+      dispatchEvent(
+        new CustomEvent(`${status.value}:actions`, {
+          detail: { action: "apply:remove", apply },
+        }),
+      );
+    }
+  },
+  { deep: true },
+);
 
 watch(() => page.value, getApplys);
 watch(() => searchTerm.value, getApplys);
@@ -105,7 +96,7 @@ async function getApplys() {
       q: searchTerm.value,
     };
 
-    result.value = await Api.$fetch<IDataResult<Apply>>("/api/admin/apply", {
+    result.value = await $fetch<IDataResult<Apply>>("/api/admin/apply", {
       query,
     });
 
@@ -124,24 +115,76 @@ async function getApplys() {
   }
 }
 
-function onEventActions(e: CustomEvent) {
-  if (e.detail.action === "apply:remove") {
-    const i = getIndex(e.detail.apply.id);
-    if (i !== null) items.value.splice(i, 1);
-  } else if (e.detail.action === "apply:add") {
-    const i = getIndex(e.detail.apply.id);
-    if (i === null) items.value.push(e.detail.apply);
-  } else if (e.detail.action === "apply:update") {
-    const i = getIndex(e.detail.apply.id);
-    if (i !== null) items.value[i] = e.detail.apply;
-  }
+function setSortable() {
+  if (!container.value) return;
+  const ul = container.value.querySelector("ul");
+
+  new Sortable(ul!, {
+    handle: ".handler",
+    group: "shared",
+    invertSwap: true,
+    sort: false,
+    disabled: Store.session.user?.role === "guest",
+    chosenClass: "chosen",
+    dragClass: "drag",
+    ghostClass: "ghost",
+    swapClass: "swap",
+    selectedClass: "selected",
+    fallbackClass: "fallback",
+
+    async onAdd(event) {
+      const id = event.item.getAttribute("data-id");
+      const to = event.to.getAttribute("data-status");
+      const from = event.from.getAttribute("data-status");
+
+      if (!id) return;
+
+      const loadID = Math.random().toString().substring(3, 20);
+      statusChanging.value[loadID] = id;
+
+      try {
+        const a0 = applys.get(id);
+        if (!a0) return;
+
+        await a0.status.value.submit(to ?? null);
+
+        const lis = ul!.querySelectorAll("li[data-id]");
+        lis.forEach((li, index) => {
+          if (li.getAttribute("data-id") === a0.apply.value.id) {
+            const is = items.value.findIndex(
+              (a) => a.id === li.getAttribute("data-id"),
+            );
+
+            if (is === -1) items.value.splice(index, 0, a0.apply.value);
+
+            dispatchEvent(
+              new CustomEvent(`${from}:actions`, {
+                detail: { action: "apply:remove", apply: result },
+              }),
+            );
+          }
+        });
+      } catch (error) {
+        onFetchError(error);
+      } finally {
+        delete statusChanging.value[loadID];
+      }
+    },
+  });
 }
 
-onBeforeUnmount(() => {
-  window.removeEventListener(`${status}:actions`, (e: any) =>
-    onEventActions(e),
-  );
-});
+function onEventActions(e: CustomEvent) {
+  if (e.detail.action === "apply:remove") {
+    const i = items.value.findIndex((a) => a.id === e.detail.apply.id);
+    if (i !== -1) items.value.splice(i, 1);
+  } else if (e.detail.action === "apply:add") {
+    const i = items.value.findIndex((a) => a.id === e.detail.apply.id);
+    if (i === -1) items.value.push(e.detail.apply);
+  } else if (e.detail.action === "apply:update") {
+    const i = items.value.findIndex((a) => a.id === e.detail.apply.id);
+    if (i !== -1) items.value[i] = e.detail.apply;
+  }
+}
 </script>
 
 <template>
@@ -151,8 +194,8 @@ onBeforeUnmount(() => {
     v-model:status="status"
   >
     <div
-      ref="container"
       class="overflow-hidden relative group rounded mb-3"
+      ref="container"
       :data-grid="status"
       :data-group="status"
       :data-status="status"
@@ -354,7 +397,6 @@ onBeforeUnmount(() => {
         </div>
 
         <ul
-          ref="sortableContainer"
           class="flex flex-col gap-2 flex-1 pt-3 pb-2 relative"
           :data-status="status"
         >
