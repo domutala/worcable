146f9ee208d520cc24989bfc122e408c58a3bc39
Mamadou Talla DIA <ibntalla@gmail.com>
2026-04-06 18:06:18 +0000

    Correction des types de données dans Soon.vue et ajout de la traduction pour le contenu de la section "soon" dans en.json

--- docs/app/components/content/Soon.vue
+++ docs/app/components/content/Soon.vue
@@ -1,20 +1,16 @@
 <script lang="ts" setup>
-import type { DocsCollectionItem } from "@nuxt/content";
+import type { DocsEnCollectionItem } from "@nuxt/content";
 
-defineProps<{ page: DocsCollectionItem }>();
+defineProps<{ page: DocsEnCollectionItem }>();
 const { toc } = useAppConfig();
 </script>
 
 <template>
   <div class="text-center">
-    <u-icon name="i-lucide-hammer" class="size-16 opacity-50" />
+    <u-icon name="i-lucide-hammer" class="size-16 opacity-30" />
 
     <p class="text-xl mt-5">
-      Nous travaillons activement à la rédaction de cette section pour vous
-      offrir les meilleures ressources possibles. Vous avez une expertise sur le
-      sujet ? Votre aide est la bienvenue ! Contribuez directement à
-      l'amélioration de ce guide en soumettant une Pull Request ou en partageant
-      vos suggestions.
+      {{ $t("content.soon.text") }}
     </p>
 
     <u-button
