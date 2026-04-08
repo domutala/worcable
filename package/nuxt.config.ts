5653ea164101a573647f85b94e0b01fd4f53c284
Mamadou Talla DIA <ibntalla@gmail.com>
2026-04-08 17:06:17 +0000

    50

--- package/nuxt.config.ts
+++ package/nuxt.config.ts
@@ -2,8 +2,6 @@ import pkg from "./package.json";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  typescript: { typeCheck: false },
-
   runtimeConfig: {
     databaseUrl: "",
     uploadsKey: "",
